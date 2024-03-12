ac0bb45899d174119b6fa680b8c5a29247fc95e2
Naelerd <juampi_22_55@hotmail.com>
2024-03-11 21:27:37 -0300

    Commit 1.0

---
+++ src/main/java/com/serviceMatch/serviceMatch/ServiceMatchApplication.java
@@ -0,0 +1,13 @@
+package com.serviceMatch.serviceMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviceMatch/serviceMatch/ServiceMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceMatch.serviceMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
